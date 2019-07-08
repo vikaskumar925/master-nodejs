bb99785e82a798d66d1a098eb5f7b4d0913c6326
Vikas Kumar <nego.vikash925@gmail.com>
2019-07-08 11:09:46 +0530

    Parsing the request object

--- Restful-API/index.js
+++ Restful-API/index.js
@@ -4,12 +4,24 @@
  */
 
 // Dependencies
-
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
+
+    //Get the url and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    //Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/|\/+$/g, '');
+
+    //send the repsonse
     res.end("hello world\n");
+
+    //log the request path
+    console.log("Request recieved on path " + trimmedPath);
 });
 
 // Start the server, and have it listen on port 3000
