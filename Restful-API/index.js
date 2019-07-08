f23942cbd02b97a3c4092c469a20ce61610ea1b5
Vikas Kumar <nego.vikash925@gmail.com>
2019-07-08 10:54:08 +0530

    Starting server

---
+++ Restful-API/index.js
@@ -0,0 +1,18 @@
+/**
+ * Primary file for API
+ * 
+ */
+
+// Dependencies
+
+var http = require('http');
+
+// The server should respond to all requests with a string
+var server = http.createServer(function(req, res) {
+    res.end("hello world\n");
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, function() {
+    console.log("The server is listening on port 3000 now");
+});
